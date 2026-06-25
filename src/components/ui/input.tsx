abe3d0ca45e85be2f10a7ae74c672f7e2858611e
aakashsharma7 <aakashsharma.ghd@gmail.com>
2026-06-25 21:19:32 +0530

    Update input component to Bauhaus Terminal style

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,9 +1,7 @@
 import * as React from "react"
-
 import { cn } from "@/lib/utils"
 
-export interface InputProps
-  extends React.InputHTMLAttributes<HTMLInputElement> {}
+export interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {}
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
   ({ className, type, ...props }, ref) => {
@@ -11,7 +9,9 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
       <input
         type={type}
         className={cn(
-          "flex h-10 w-full rounded-lg border border-slate-600 bg-slate-800/50 px-3 py-2 text-sm text-slate-200 ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-slate-500 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2 focus-visible:ring-offset-slate-900 focus-visible:border-blue-500 disabled:cursor-not-allowed disabled:opacity-50 transition-all duration-300 hover:border-slate-500 hover:bg-slate-800/70",
+          "flex w-full term-input",
+          "ring-offset-transparent",
+          "file:border-0 file:bg-transparent file:font-mono file:text-[11px] file:text-[#E8E4D0]",
           className
         )}
         ref={ref}
